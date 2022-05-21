c8096a2b5b16b3d3d8767cf565b10f35bf592dbe
Philip Sutton <philip@sutton.pl>
2022-05-21 13:24:12 +0200

    added tests

--- cart_test_no_user.py
+++ cart_test_no_user.py
@@ -1,38 +1,37 @@
-# import time
-# import unittest
-# from selenium.webdriver.support import expected_conditions as EC
-#
-# from selenium import webdriver
-# from selenium.webdriver import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-# from xmlrunner import xmlrunner
-#
-#
-# class CartTestsNoUser(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = webdriver.Firefox()
-#         self.driver.get('https://demo.opencart.com')
-#
-#     def test_add_item_to_cart(self):
-#         driver = self.driver
-#
-#         def item_added():
-#             try:
-#                 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Desktops"))).click()
-#                 driver.find_element(By.XPATH, "//a[starts-with(text(),'Mac')]").click()
-#                 driver.find_element(By.XPATH, "//span[contains(text(),'Add to Cart')]").click()
-#                WebDriverWait(driver, 5).until(
-#                    EC.presence_of_element_located((By.XPATH, "//td[contains(text(),'122')]")))
-                # return True
-            # except:
-            #     return False
-        # self.assertTrue(item_added(), "Error")
-    #
-    # def tearDown(self):
-    #     self.driver.close()
-#
-#
-# if __name__ == '__main__':
-#     unittest.main(testRunner=xmlrunner.XMLTestRunner(output = 'reports'),failfast=False,buffer=False,catchbreak=False)
+import time
+import unittest
+from selenium.webdriver.support import expected_conditions as EC
 
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from xmlrunner import xmlrunner
+
+
+class CartTestsNoUser(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('https://demo.opencart.com')
+
+    def test_add_item_to_cart(self):
+        driver = self.driver
+
+        def item_added():
+            try:
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Desktops"))).click()
+                driver.find_element(By.XPATH, "//a[starts-with(text(),'Mac')]").click()
+                driver.find_element(By.XPATH, "//span[contains(text(),'Add to Cart')]").click()
+#               WebDriverWait(driver, 5).until(
+#                  EC.presence_of_element_located((By.XPATH, "//td[contains(text(),'122')]")))
+                return True
+            except:
+                return False
+        self.assertTrue(item_added(), "Error")
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output = 'reports'),failfast=False,buffer=False,catchbreak=False)
--- cart_test_user.py
+++ cart_test_user.py
@@ -1,36 +1,35 @@
-# import time
-# import unittest
-#
-# import xmlrunner
-# from selenium.webdriver.support import expected_conditions as EC
-# from cart_test_no_user import CartTestsNoUser
-# from selenium import webdriver
-# from selenium.webdriver import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-#
-# class CartTestsUser(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = webdriver.Firefox()
-#         self.driver.get('https://demo.opencart.com')
-#
-#     def test_add_item_to_cart(self):
-#         driver = self.driver
-#         def login():
-#             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "My Account"))).click()
-#             driver.find_element(By.LINK_TEXT, "Login").click()
-#             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-email"))).send_keys(
-#                 'philip@example.com')
-#             driver.find_element(By.ID, "input-password").send_keys('testpassword' + Keys.ENTER)
-#             WebDriverWait(driver, 5).until(
-#                 EC.presence_of_element_located((By.XPATH, "//a[text()= 'Edit your account information']")))
-#         login()
-#         CartTestsNoUser.test_add_item_to_cart(self)
-#
-#     def tearDown(self):
-#         self.driver.close()
-#
-#
-# if __name__ == '__main__':
-#     unittest.main(testRunner=xmlrunner.XMLTestRunner(output = 'reports'),failfast=False,buffer=False,catchbreak=False)
-#
+import time
+import unittest
+
+import xmlrunner
+from selenium.webdriver.support import expected_conditions as EC
+from cart_test_no_user import CartTestsNoUser
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+class CartTestsUser(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('https://demo.opencart.com')
+
+    def test_add_item_to_cart(self):
+        driver = self.driver
+        def login():
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "My Account"))).click()
+            driver.find_element(By.LINK_TEXT, "Login").click()
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-email"))).send_keys(
+                'philip@example.com')
+            driver.find_element(By.ID, "input-password").send_keys('testpassword' + Keys.ENTER)
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, "//a[text()= 'Edit your account information']")))
+        login()
+        CartTestsNoUser.test_add_item_to_cart(self)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output = 'reports'),failfast=False,buffer=False,catchbreak=False)
